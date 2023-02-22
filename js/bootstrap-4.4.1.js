9e91bb0a12dbe1abdebcc5aba5a781c89d165926
Wesley Li <orangewesli@gmail.com>
2023-02-22 14:55:58 -0800

    added all extra pages (6)

--- js/bootstrap-4.4.1.js
+++ js/bootstrap-4.4.1.js
@@ -4519,3 +4519,19 @@
 
 })));
 //# sourceMappingURL=bootstrap.js.map
+
+
+  /**
+   * ------------------------------------------------------------------------
+   * Custom
+   * ------------------------------------------------------------------------
+   */
+
+   function hide(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
